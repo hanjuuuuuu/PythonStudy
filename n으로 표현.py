206764e6602a0dc66dcc11680c030c491478c9be
hanjuuuuuu <hanjucomss@naver.com>
2024-07-03 23:47:31 +0900

    dp lv.3

---
+++ n으로 표현.py
@@ -0,0 +1,20 @@
+def solution(N, number):
+    # 5부터 55555555까지 저장
+    dp = []
+    for i in range(1, 9):
+        dp.append({int(str(N) * i)})
+
+    for i in range(8):
+        for j in range(i):
+            for num1 in dp[j]:
+                for num2 in dp[i-j-1]:
+                    dp[i].add(num1 + num2)
+                    dp[i].add(num1 - num2)
+                    dp[i].add(num1 * num2)
+                    if num2 != 0:
+                        dp[i].add(num1 // num2)
+        if number in dp[i]:
+            return i+1
+    return -1
+
+solution(5, 12)
