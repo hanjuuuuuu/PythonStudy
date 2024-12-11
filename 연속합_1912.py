964603121e2d911e722f7343fdb962fd72643ea3
hanjuuuuuu <hanjucomss@naver.com>
2024-12-11 23:47:45 +0900

    누적합

---
+++ 연속합_1912.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+prefix = [0 for _ in range(n+1)]
+
+for i in range(n):
+    prefix[i+1] = max(prefix[i] + arr[i], arr[i]) 
+
+print(max(prefix))
