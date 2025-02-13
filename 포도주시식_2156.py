95c58c8c5c8e4f89108a4d21c519bccc860d3e49
hanjuuuuuu <hanjucomss@naver.com>
2025-02-13 23:57:17 +0900

    다이나믹 프로그래밍

---
+++ 포도주시식_2156.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline 
+
+n = int(input())
+wine = [0]
+for _ in range(n):
+    wine.append(int(input()))
+
+dp = [0] * (n+1)
+dp[1] = wine[1]
+dp[2] = wine[1] + wine[2]
+for i in range(3, n+1):
+    dp[i] = max(dp[i-2]+wine[i], dp[i-1], dp[i-3]+wine[i-1]+wine[i])
+
+print(dp[n])
